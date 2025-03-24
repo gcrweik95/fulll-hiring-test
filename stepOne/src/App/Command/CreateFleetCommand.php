d931220ac84269fbf06ab62bdb719ae86ff659ad
Gerard <gc.rweik95@gmail.com>
2025-03-24 14:49:45 +0100

    Step One CI Test

--- stepOne/src/App/Command/CreateFleetCommand.php
+++ stepOne/src/App/Command/CreateFleetCommand.php
@@ -5,7 +5,6 @@ namespace Fulll\App\Command;
 class CreateFleetCommand
 {
     private string $fleetId;
-    // This is a test
     public function __construct(string $fleetId)
     {
         $this->fleetId = $fleetId;
